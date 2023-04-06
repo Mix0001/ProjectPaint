ec2951d770cbb90a617005c528956fff7d22a618
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2023-04-06 13:52:02 +0700

    จัด layout

--- main.c
+++ main.c
@@ -11,12 +11,11 @@
 #define Delete01 5
 #define DRAW_Lines 6
 
-
 int mainMenu, colorMenu, drawMenu;
 int X, Y, r, g, b;
-int checkCase=0;
-int aa=0,sizeDelete=10,sizeTri=50,sizeRac=50,bb=0,first;
-int Rx=0,Ry=0;
+int checkCase = 0;
+int aa = 0, sizeDelete = 10, sizeTri = 50, sizeRac = 50, bb = 0, first;
+int Rx = 0, Ry = 0;
 int flipX = 800, flipY = 500;
 void display();
 void menuFunc();
@@ -25,71 +24,69 @@ void init()
     glClearColor(1.0, 1.0, 1.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(0.0, flipX, flipY, 0.0);
-
 }
 
 void drawSubMenu(int option)
 {
-// Handle submenu options for drawing
-    switch(option)
+    // Handle submenu options for drawing
+    switch (option)
     {
     case DRAW_POINT:
-        checkCase=0;
+        checkCase = 0;
         break;
     case DRAW_CIRCLE:
-        checkCase=1;
+        checkCase = 1;
         break;
     case DRAW_TRIANGLE:
-        checkCase=3;
+        checkCase = 3;
         break;
     case DRAW_RACTANGLE:
-        checkCase=4;
+        checkCase = 4;
         break;
     case DRAW_Lines:
-        checkCase=6;
+        checkCase = 6;
         break;
-
     }
 }
 void colorSubMenu(int option)
 {
-    switch(option)
+    switch (option)
     {
     case 1:
         glColor3f(1.0, 0.0, 0.0); // red
-        r=1;
-        g=0;
-        b=0;
+        r = 1;
+        g = 0;
+        b = 0;
         break;
     case 2:
         glColor3f(0.0, 1.0, 0.0); // green
-        r=0;
-        g=1;
-        b=0;
+        r = 0;
+        g = 1;
+        b = 0;
         break;
     case 3:
         glColor3f(0.0, 0.0, 1.0); // blue
-        r=0;
-        g=0;
-        b=1;
+        r = 0;
+        g = 0;
+        b = 1;
         break;
     case 4:
         glColor3f(0.0, 0.0, 0.0); // blue
-        r=0;
-        g=0;
-        b=0;
+        r = 0;
+        g = 0;
+        b = 0;
         break;
     }
 }
 
 void mainMenuFunc(int option)
 {
-    switch(option)
+    switch (option)
     {
     case Delete01:
-        checkCase=2;
+        checkCase = 2;
         break;
-//        // Add more cases for other menu options as needed
+        //        // Add more cases for other menu options as needed
     }
 }
 
@@ -113,121 +110,123 @@ void createMenu()
     glutAddSubMenu("Draw", drawMenu);
     glutAddMenuEntry("Delete", Delete01);
 
-
     glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 void plotQ(int x, int y, int xcenter, int ycenter)
 {
-    glVertex2d(x + xcenter, y + ycenter);//Q1
-    glVertex2d(y + xcenter, x + ycenter);//Q2
-    glVertex2d(y + xcenter, -x + ycenter);//Q3
-    glVertex2d(x + xcenter, -y + ycenter);//Q4
-    glVertex2d(-x + xcenter, -y + ycenter);//Q5
-    glVertex2d(-y + xcenter, -x + ycenter);//Q6
-    glVertex2d(-y + xcenter, x + ycenter);//Q7
-    glVertex2d(-x + xcenter, y + ycenter);//Q8
+    glVertex2d(x + xcenter, y + ycenter);   // Q1
+    glVertex2d(y + xcenter, x + ycenter);   // Q2
+    glVertex2d(y + xcenter, -x + ycenter);  // Q3
+    glVertex2d(x + xcenter, -y + ycenter);  // Q4
+    glVertex2d(-x + xcenter, -y + ycenter); // Q5
+    glVertex2d(-y + xcenter, -x + ycenter); // Q6
+    glVertex2d(-y + xcenter, x + ycenter);  // Q7
+    glVertex2d(-x + xcenter, y + ycenter);  // Q8
 }
-void DDD(X1,Y1,X2,Y2){
+void DDD(X1, Y1, X2, Y2)
+{
     double x1 = X1, y1 = Y1, x2 = X2, y2 = Y2;
-    float dx = (x2-x1);
-    float dy = (y2-y1);
+    float dx = (x2 - x1);
+    float dy = (y2 - y1);
     float step, xinc, yinc, x, y;
 
-    if(abs(dx) > abs(dy)){
+    if (abs(dx) > abs(dy))
+    {
         step = abs(dx);
     }
-    else{
+    else
+    {
         step = abs(dy);
     }
 
-    xinc = dx/(float)step;
-    yinc = dy/(float)step;
+    xinc = dx / (float)step;
+    yinc = dy / (float)step;
 
     x = x1;
     y = y1;
 
-    glColor3f(r,g,b);
+    glColor3f(r, g, b);
     glPointSize(2);
 
-    for(int i = 0; i < step; i++){
-        x+=xinc;
-        y+=yinc;
+    for (int i = 0; i < step; i++)
+    {
+        x += xinc;
+        y += yinc;
         glBegin(GL_POINTS);
-        glVertex2d(round(x),round(y));
+        glVertex2d(round(x), round(y));
         glEnd();
         glFlush();
     }
-
 }
 void draw()
 {
-    if(checkCase==0)
+    if (checkCase == 0)
     {
         glPointSize(2);
-        glColor3f(r,g,b);
+        glColor3f(r, g, b);
         glBegin(GL_POINTS);
         glVertex2i(X, Y);
         glEnd();
         glFlush();
     }
-    else if(checkCase==1)
+    else if (checkCase == 1)
     {
-        int r2=0;
-        if (aa>0)
+        int r2 = 0;
+        if (aa > 0)
         {
-            aa=0;
-            if(Rx>X)
+            aa = 0;
+            if (Rx > X)
             {
-                r2=Rx-X;
+                r2 = Rx - X;
             }
             else
             {
-                r2=X-Rx;
+                r2 = X - Rx;
             }
             int xcenter = X, ycenter = Y;
             int x = 0, y = r2;
-            int p = 1-r2;
-            glColor3f(r,g,b);
+            int p = 1 - r2;
+            glColor3f(r, g, b);
             glPointSize(2);
             glBegin(GL_POINTS);
             glVertex2d(x + xcenter, y + ycenter);
             glEnd();
-            while(x<y)
+            while (x < y)
             {
                 x++;
-                if(p<0)
+                if (p < 0)
                 {
-                    p = p+2*x+1;
+                    p = p + 2 * x + 1;
                 }
                 else
                 {
                     y--;
-                    p = p+2*x+1-2*y;
+                    p = p + 2 * x + 1 - 2 * y;
                 }
                 glBegin(GL_POINTS);
-                plotQ(x,y,xcenter,ycenter);
+                plotQ(x, y, xcenter, ycenter);
                 glEnd();
                 glFlush();
             }
         }
         else
         {
-            Rx=X;
-            Ry=Y;
+            Rx = X;
+            Ry = Y;
             aa++;
         }
     }
-    else if(checkCase==2)
+    else if (checkCase == 2)
     {
         glPointSize(sizeDelete);
-        glColor3f(1,1,1);
+        glColor3f(1, 1, 1);
         glBegin(GL_POINTS);
         glVertex2i(X, Y);
         glEnd();
         glFlush();
     }
-    else if (checkCase==3){
-
+    else if (checkCase == 3)
+    {
 
         /*glBegin(GL_LINE_STRIP);
         glColor3f(r,g,b);
@@ -238,36 +237,37 @@ void draw()
         glEnd();
         glFlush();*/
 
-        DDD(X,Y-sizeTri,X-sizeTri,Y+sizeTri);
-        DDD(X-sizeTri,Y+sizeTri,X+sizeTri,Y+sizeTri);
-        DDD(X+sizeTri,Y+sizeTri,X,Y-sizeTri);
-
+        DDD(X, Y - sizeTri, X - sizeTri, Y + sizeTri);
+        DDD(X - sizeTri, Y + sizeTri, X + sizeTri, Y + sizeTri);
+        DDD(X + sizeTri, Y + sizeTri, X, Y - sizeTri);
     }
-    else if (checkCase==4){
+    else if (checkCase == 4)
+    {
 
-        DDD(X-sizeRac,Y-sizeRac,X-sizeRac,Y+sizeRac);
-        DDD(X-sizeRac,Y+sizeRac,X+sizeRac,Y+sizeRac);
-        DDD(X+sizeRac,Y+sizeRac,X+sizeRac,Y-sizeRac);
-        DDD(X+sizeRac,Y-sizeRac,X-sizeRac,Y-sizeRac);
+        DDD(X - sizeRac, Y - sizeRac, X - sizeRac, Y + sizeRac);
+        DDD(X - sizeRac, Y + sizeRac, X + sizeRac, Y + sizeRac);
+        DDD(X + sizeRac, Y + sizeRac, X + sizeRac, Y - sizeRac);
+        DDD(X + sizeRac, Y - sizeRac, X - sizeRac, Y - sizeRac);
     }
-    else if(checkCase==6){
+    else if (checkCase == 6)
+    {
 
-        if(bb==1){
+        if (bb == 1)
+        {
             printf("111");
-            bb=0;
-            DDD(Rx,Ry,X,Y);
+            bb = 0;
+            DDD(Rx, Ry, X, Y);
         }
-        else{
+        else
+        {
             printf("222");
-            Rx=X;
-            Ry=Y;
-            bb=1;
+            Rx = X;
+            Ry = Y;
+            bb = 1;
         }
-
     }
-
 }
-void mouse(int button, int state, int x, int y) //จับการคลิกของเมาส์
+void mouse(int button, int state, int x, int y) // จับการคลิกของเมาส์
 {
 
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
@@ -281,10 +281,9 @@ void mouse(int button, int state, int x, int y) //จับการคลิก
     {
         printf("(%d,%d)\n", X, Y);
     }
-
 }
 
-void motion(int x, int y) //จับการเคลื่อนไหวของเมาส์
+void motion(int x, int y) // จับการเคลื่อนไหวของเมาส์
 {
     X = x;
     Y = y;
@@ -298,26 +297,33 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT);
     glFlush();
 }
-void keyboard(unsigned char key,int x,int y){
-    if(key=='+'&&checkCase==2){
-        sizeDelete+=2;
+void keyboard(unsigned char key, int x, int y)
+{
+    if (key == '+' && checkCase == 2)
+    {
+        sizeDelete += 2;
     }
-    if(key=='-'&checkCase==2){
-        sizeDelete-=2;
+    if (key == '-' & checkCase == 2)
+    {
+        sizeDelete -= 2;
     }
-    if(key=='+'&&checkCase==3){
-        sizeTri+=2;
+    if (key == '+' && checkCase == 3)
+    {
+        sizeTri += 2;
     }
-    if(key=='-'&checkCase==3){
-        sizeTri-=2;
+    if (key == '-' & checkCase == 3)
+    {
+        sizeTri -= 2;
     }
-    if(key=='+'&&checkCase==4){
-        sizeRac+=2;
+    if (key == '+' && checkCase == 4)
+    {
+        sizeRac += 2;
     }
-    if(key=='-'&checkCase==4){
-        sizeRac-=2;
+    if (key == '-' & checkCase == 4)
+    {
+        sizeRac -= 2;
     }
-    //glutPostRedisplay();
+    // glutPostRedisplay();
 }
 int main(int argc, char **argv)
 {
